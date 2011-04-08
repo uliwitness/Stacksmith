e381e79d56bbfed92422f91cb09b6ac2854e27b2
Uli Kusterer <ulivc@zathras.de>
2011-04-09 01:27:38 +0200

    WILDButtonView now overrides mouseDown: and runs its own tracking loop so it can do correct hit-testing, not the wrong type-specific kind NSButton does even with the custom cell.

--- WILDButtonView.m
+++ WILDButtonView.m
@@ -23,9 +23,54 @@
 
 -(void)	mouseDown: (NSEvent*)event
 {
-	WILDPartView*	pv = [self superview];
-	if( [[pv part] autoHighlight] )
-		[super mouseDown: event];
+	WILDPartView*			pv = [self superview];
+	BOOL					keepLooping = YES;
+	BOOL					autoHighlight = [[pv part] autoHighlight];
+	BOOL					isInside = YES;
+	
+	if( autoHighlight )
+		[[self cell] setHighlighted: YES];
+	NSAutoreleasePool	*	pool = [[NSAutoreleasePool alloc] init];
+	
+	while( keepLooping )
+	{
+		NSEvent	*	evt = [NSApp nextEventMatchingMask: NSLeftMouseUpMask | NSRightMouseUpMask | NSOtherMouseUpMask | NSLeftMouseDraggedMask | NSRightMouseDraggedMask | NSOtherMouseDraggedMask untilDate: [NSDate distantFuture] inMode: NSEventTrackingRunLoopMode dequeue: YES];
+		if( evt )
+		{
+			switch( [evt type] )
+			{
+				 case NSLeftMouseUp:
+				 case NSRightMouseUp:
+				 case NSOtherMouseUp:
+					keepLooping = NO;
+					break;
+				
+				case NSLeftMouseDragged:
+				case NSRightMouseDragged:
+				case NSOtherMouseDragged:
+					if( isInside != ([self hitTest: [self convertPoint: [evt locationInWindow] fromView: nil]] != nil) )
+					{
+						isInside = !isInside;
+						
+						if( autoHighlight )
+							[[self cell] setHighlighted: isInside];
+					}
+					break;
+			}
+		}
+		
+		[pool release];
+		pool = [[NSAutoreleasePool alloc] init];
+	}
+	
+	if( isInside )
+	{
+		if( autoHighlight )
+			[[self cell] setHighlighted: NO];
+		[[self target] performSelector: [self action]];
+	}
+	
+	[pool release];
 }
 
 
