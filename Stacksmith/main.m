5243f8f6f0b37fa8fdf199e0f74be7feb78f59bb
Uli Kusterer <ulivc@zathras.de>
2012-04-15 14:01:38 +0200

    Preparing to re-arrange repository for adding subprojects with dependencies

---
+++ Stacksmith/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Propaganda
+//
+//  Created by Uli Kusterer on 27.02.10.
+//  Copyright 2010 The Void Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	srand( time(NULL) );
+	
+    return NSApplicationMain(argc, (const char **) argv);
+}
