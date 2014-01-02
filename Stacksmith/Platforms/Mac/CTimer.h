23fa867f9b871b3093e78b55b15cf4cbe30860fa
Uli Kusterer <ulivc@zathras.de>
2014-01-02 06:53:11 +0100

    Add a wrapper for Mac timers which our cross-platform document code can then use.

---
+++ Stacksmith/Platforms/Mac/CTimer.h
@@ -0,0 +1,58 @@
+//
+//  CTimer.h
+//  Stacksmith
+//
+//  Created by Uli Kusterer on 2014-01-02.
+//  Copyright (c) 2014 Uli Kusterer. All rights reserved.
+//
+
+#ifndef __Stacksmith__CTimer__
+#define __Stacksmith__CTimer__
+
+/*
+	A simple timer class that calls its handler at roughly the given
+	frequency. Timers start out stopped, and stop themselves when destructed.
+*/
+
+#include <functional>
+
+#if __OBJC__
+#include <Foundation/Foundation.h>
+
+@class CTimerMacBridge;
+
+typedef NSTimer*				NSTimerPtr;
+typedef CTimerMacBridge*		CTimerMacBridgePtr;
+#else
+typedef struct NSTimer*			NSTimerPtr;
+typedef struct CTimerMacBridge*	CTimerMacBridgePtr;
+#endif
+
+
+namespace Calhoun {
+
+class CTimer
+{
+public:
+	explicit CTimer( long long inTimeIntervalInTicks = 60 );
+	~CTimer();
+	
+	void		Start();
+	void		Stop();
+	bool		IsRunning()		{ return mMacTimer != NULL; };
+	
+	void		SetInterval( long long inTimeIntervalInTicks );
+	void		SetHandler( std::function<void(CTimer*)> timerHandler )	{ mHandler = timerHandler; };
+	
+	void		Trigger()		{ if( mHandler ) mHandler( this ); };	// Called by Mac-specific code, do not use!
+	
+protected:
+	NSTimerPtr						mMacTimer;
+	CTimerMacBridgePtr				mMacTarget;
+	std::function<void(CTimer*)>	mHandler;
+	long long						mTimeIntervalInTicks;
+};
+
+}
+
+#endif /* defined(__Stacksmith__CTimer__) */
---
+++ Stacksmith/Platforms/Mac/CTimer.mm
@@ -0,0 +1,91 @@
+//
+//  CTimer.mm
+//  Stacksmith
+//
+//  Created by Uli Kusterer on 2014-01-02.
+//  Copyright (c) 2014 Uli Kusterer. All rights reserved.
+//
+
+#include "CTimer.h"
+
+
+using namespace Calhoun;
+
+
+@interface CTimerMacBridge : NSObject
+{
+	CTimer	*		owningTimer;
+}
+
+-(id)	initWithOwningCppTimer: (CTimer*)inOwner;
+
+-(void)	timerAction: (NSTimer*)sender;
+
+@end
+
+
+@implementation CTimerMacBridge : NSObject
+
+-(id)	initWithOwningCppTimer: (CTimer*)inOwner
+{
+	self = [super init];
+	if( self )
+		owningTimer = inOwner;
+	return self;
+}
+
+
+-(void)	timerAction: (NSTimer*)sender
+{
+	owningTimer->Trigger();
+}
+
+@end
+
+
+
+CTimer::CTimer( long long inTimeIntervalInTicks )
+	: mMacTimer(nil), mMacTarget(nil), mHandler(NULL), mTimeIntervalInTicks(inTimeIntervalInTicks)
+{
+
+}
+	
+
+CTimer::~CTimer()
+{
+	if( mMacTimer )
+		Stop();
+}
+
+
+void	CTimer::Start()
+{
+	if( !mMacTimer )
+	{
+		mMacTarget = [[CTimerMacBridge alloc] initWithOwningCppTimer: this];
+		mMacTimer = [[NSTimer alloc] initWithFireDate: [NSDate date] interval: ((NSTimeInterval)mTimeIntervalInTicks) / 60.0 target: mMacTarget selector: @selector(timerAction:) userInfo: nil repeats: YES];
+	}
+}
+
+
+void	CTimer::Stop()
+{
+	[mMacTimer invalidate];
+	[mMacTimer release];
+	mMacTimer = nil;
+	[mMacTarget release];
+	mMacTarget = nil;
+}
+
+
+void	CTimer::SetInterval( long long inTimeIntervalInTicks )
+{
+	mTimeIntervalInTicks = inTimeIntervalInTicks;
+	
+	if( mMacTimer )
+	{
+		Stop();
+		Start();
+	}
+}
+
