eb8ba113e7e4487e62204810a02a5e9dae5f98be
Uli Kusterer <ulivc@zathras.de>
2014-01-12 21:08:35 +0100

    Have movie player honor its shadow setting.

--- Stacksmith/Platforms/Mac/CStackMac.mm
+++ Stacksmith/Platforms/Mac/CStackMac.mm
@@ -192,10 +192,10 @@ public:
 		mView = [[ULIInvisiblePlayerView alloc] initWithFrame: NSMakeRect(mLeft, mTop, mRight -mLeft, mBottom -mTop)];
 		[mView setWantsLayer: YES];
 		mView.layer.masksToBounds = NO;
-		[mView.layer setShadowColor: [NSColor.redColor CGColor]];
-		[mView.layer setShadowOffset: CGSizeMake(4, -4)];
-		[mView.layer setShadowRadius: 8];
-		[mView.layer setShadowOpacity: 1.0];
+		[mView.layer setShadowColor: [NSColor colorWithCalibratedRed: (mShadowColorRed / 65535.0) green: (mShadowColorGreen / 65535.0) blue: (mShadowColorBlue / 65535.0) alpha:(mShadowColorAlpha / 65535.0)].CGColor];
+		[mView.layer setShadowOffset: CGSizeMake(mShadowOffsetWidth, -mShadowOffsetHeight)];
+		[mView.layer setShadowRadius: mShadowBlurRadius];
+		[mView.layer setShadowOpacity: mShadowColorAlpha == 0 ? 0.0 : 1.0];
 		mView.player = [AVPlayer playerWithURL: [[NSBundle mainBundle] URLForResource: @"PlaceholderMovie" withExtension: @"mov"]];
 		[inSuperView addSubview: mView];
 	};
