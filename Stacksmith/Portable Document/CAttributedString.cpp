4b77d177e16440318eb6bf751347e819816a72f5
Uli Kusterer <ulivc@zathras.de>
2014-02-27 23:55:45 +0100

    We should now be able to convert back and forth between UTF16 offsets (as the Mac UI uses them) and UTF8 offsets (as Stacksmith uses them internally).

--- Stacksmith/Portable Document/CAttributedString.cpp
+++ Stacksmith/Portable Document/CAttributedString.cpp
@@ -532,6 +532,46 @@ size_t	CAttributedString::UTF16OffsetFromUTF8Offset( size_t inOffs ) const
 }
 
 
+/*static*/ size_t	CAttributedString::UTF8OffsetFromUTF16Offset( size_t inCharOffs, const uint16_t* utf16, size_t byteLen )
+{
+	size_t		currOffs = 0;
+	size_t		currUTF8Offs = 0;
+	
+	if( inCharOffs == 0 )
+		return 0;
+	
+	while( (currOffs * sizeof(uint16_t)) < byteLen )
+	{
+		uint32_t	currCh = UTF16StringParseUTF32CharacterAtOffset( utf16, byteLen, &currOffs );
+		currUTF8Offs += UTF8LengthForUTF32Char( currCh );
+		if( currOffs >= inCharOffs )
+			break;
+	}
+	
+	return currUTF8Offs;
+}
+
+
+/*static*/ size_t	CAttributedString::UTF16OffsetFromUTF8Offset( size_t inOffs, const uint8_t* utf8, size_t byteLen )
+{
+	size_t		currOffs = 0;
+	size_t		currUTF16Offs = 0;
+	
+	if( inOffs == 0 )
+		return 0;
+	
+	while( currOffs < byteLen )
+	{
+		uint32_t	currCh = UTF8StringParseUTF32CharacterAtOffset( (char*) utf8, byteLen, &currOffs );
+		currUTF16Offs += UTF16LengthForUTF32Char( currCh );
+		if( currOffs >= inOffs )
+			break;
+	}
+	
+	return currUTF16Offs;
+}
+
+
 size_t	CAttributedString::UTF32OffsetFromUTF8Offset( size_t inOffs ) const
 {
 	const char*	str = GetString().c_str();
